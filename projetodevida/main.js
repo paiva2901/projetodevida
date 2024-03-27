13b852ed7dd8d578710af98e47887cac8c9cd1e6
paiva2901 <106085288+paiva2901@users.noreply.github.com>
2024-03-27 14:03:35 -0300

    arrumandojs

--- projetodevida/main.js
+++ projetodevida/main.js
@@ -6,4 +6,7 @@ botoes[j].classList.remove("ativo");
 }
 botoes[i].classList.add("ativo");
 }
-}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
